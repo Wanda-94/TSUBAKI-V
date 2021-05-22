912720410b2e3e3ae11f929e7c326798a0289af2
Wanda-94 <71314370+Wanda-94@users.noreply.github.com>
2021-05-22 14:09:01 +0800

    update source

--- TSUBAKI-V/TSUBAKI-V.cpp
+++ TSUBAKI-V/TSUBAKI-V.cpp
@@ -2,11 +2,29 @@
 //
 
 #include "TSUBAKI-V.h"
-
-using namespace std;
+#include "vulkan/vulkan.hpp"
+#include "GLFW/glfw3.h"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	//GLFWwindow* window = glfwCreateWindow(1080,1920,"TSUBAKI-V",nullptr,nullptr);
+	//while (!glfwWindowShouldClose(window))
+	//{
+	//	cout << "run" << endl;
+	//}
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API,GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800,600,"Vulkan",nullptr,nullptr);
+	uint32_t extension_count = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr,&extension_count,nullptr);
+
+	std::cout << "extensions supported " << extension_count << std::endl;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
